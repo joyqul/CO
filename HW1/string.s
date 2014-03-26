b7e403fe2a3df9174463091a086a775388570f3b
Joyqul Chen <joy1357924680@gmail.com>
2014-03-26 20:34:00 +0800

    Print string.

--- HW1/string.s
+++ HW1/string.s
@@ -1,3 +1,29 @@
 .text
 .globl main
+    main:
+    PrintInput:
+        la $a0, inprint1
+        syscall
+        la $a0, newline
+        syscall
+        la $a0, inprint2
+        syscall
+        la $a0, newline
+        syscall
+    PrintOutput:
+        la $a0, outprint1
+        syscall
+        la $a0, newline
+        syscall
+        la $a0, outprint2
+        syscall
+        la $a0, newline
+        syscall
+        jr $ra
 .data
+    str: .space 32 #offer a 32-byte space to store string
+    newline: .asciiz "\n"
+    inprint1: .asciiz "Please enter main string:"
+    inprint2: .asciiz "Please enter pattern string"
+    outprint1: .asciiz "The reversed main string:"
+    outprint2: .asciiz "The number of substrings to match pattern string:"
