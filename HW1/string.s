2ccfd06b6ece136b5e957934b89c68b6d11503a1
Joyqul Chen <joy1357924680@gmail.com>
2014-03-26 20:54:54 +0800

    Reduce the newline.

--- HW1/string.s
+++ HW1/string.s
@@ -2,28 +2,33 @@
 .globl main
     main:
     PrintInput:
+        li $v0, 4 # print string syscall
         la $a0, inprint1
         syscall
-        la $a0, newline
+
+        li $v0, 8 # read string syscall
+        la $a0, str
         syscall
+
+        li $v0, 4 # print string syscall
         la $a0, inprint2
         syscall
-        la $a0, newline
+
+        li $v0, 8 # read string syscall
+        la $a0, str
+        la $a1, 5 # limit string len as 4
         syscall
+
     PrintOutput:
+        li $v0, 4 # print string syscall
         la $a0, outprint1
         syscall
-        la $a0, newline
-        syscall
         la $a0, outprint2
-        syscall
-        la $a0, newline
         syscall
         jr $ra
 .data
     str: .space 32 #offer a 32-byte space to store string
-    newline: .asciiz "\n"
     inprint1: .asciiz "Please enter main string:"
-    inprint2: .asciiz "Please enter pattern string"
-    outprint1: .asciiz "The reversed main string:"
-    outprint2: .asciiz "The number of substrings to match pattern string:"
+    inprint2: .asciiz "\nPlease enter pattern string:"
+    outprint1: .asciiz "\nThe reversed main string:"
+    outprint2: .asciiz "\nThe number of substrings to match pattern string:"
